f10973ffa4599ea0cb88ed5fc3590238928b1ab0
Ruth John <rumyra@gmail.com>
2020-05-31 12:30:36 +0100

    mods from codevember

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
 # Vizra
 A set of libraries to generatively create canvas shapes: Still in version 0.000001
 
+## Physics
+
+A vector (VizVector) type, is used for the physics in the boid (VizBoid) which is then in turn used to build a grid of boids (VizGrid)
+
+## Facade
+
+A colour type (VizColour) is used with paths (VizPaths) to make up a tile (Viz Tile)
+
+## State
+
+Make all params global and included within master Viz - each input changes exactly the same param for each variation on the above - MIDI controllers are _not_ a different set up for each viz (a different viz is essentially a tile switch).
+
+### Pads
+- tile switch
+- colour switch & FX
+- grid switch
+
+### CCs
+- tile & path transforms
+- colour FX
+- boid physics
+- grid params
+
+### Audio
+- all of the above (velocity peak for pads & velocity val for cc)
+
 #### JS Doc Command
 
 `~Vizra/Vizra jsdoc -c docs/config.json`
--- app/app.js
+++ app/app.js
@@ -3,29 +3,33 @@ console.clear();
 import VizTile from '../lib/shape/VizTile.js'
 import Canvas from '../lib/Canvas.js';
 import paths from '../lib/shape/vizPaths.js';
+import VizGrid from '../lib/physics/VizGrid.js';
 
 // don't select this, add it to vizcanvas
 const vizScreen = new Canvas();
 
-const shape = new VizTile(100, 100, paths.circle());
+const grid = new VizGrid(vizScreen, 'grid', 'regular', false);
 
-shape.fill = 'hsla(0, 50%, 50%, 1)';
-shape.update(vizScreen.ctx);
+grid.coords.forEach((el, i) => {
+	const shape = new VizTile(el.x, el.y, paths.circle());
+	shape.fill = 'hsla(0, 50%, 50%, 1)';
+	shape.update(vizScreen.ctx);
+} )
 
-console.log(shape);
+
 
 // Ideally creating a viz will go something like this:
 
-// index file with canvas instigated, this is the global context
+// index file with canvas instigated (stage?), this is the global context
 // create module files for each viz - this is the master example
-// create colour palette (made from colours), tile (made from paths) & grid (made from vectors). Each of these has it's own set of global params, controlled by audio & midi state
-// create an array of 'Vi', with grid vector & colours & tiles
+// create colour palette (made from colours), tile (made from paths) & grid (made from boids). Each of these has it's own set of global params, controlled by audio & midi state
+// create an array of 'VizTiles', by looping over grid, using boid physics and adding a facade
 
-// draw viz by passing in array and params (loop over Vi array & draw)
+// draw viz by passing in array and params (loop over VizTiles array & draw)
 // could each viz have so much manipulation it becomes a 'set' and you switch out the viz to change set? That way you bypass 'sets' and just have 16/32 viz to switch out
-// Can each grid vector contain direction, weight & friction? What about neighbours
+// Can each grid vector contain direction, weight & friction? What about neighbours -> boid
 
-// what do you want to be able to change via params?
+// what do you want to be able to change via params? -> adding to state
 // viz
 // paths & tiles: transforms & fills for each path (maybe even the path itself: manipulate points & curves), tranforms and palette for each tile
 // colours & palettes: each individual colour change (h, s, b, a), and overall palette: hue shift, invert, brightness, greyscale
