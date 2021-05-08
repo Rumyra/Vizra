6188be0fb913ce907aa68b9767a54d80b96c8c15
Rumyra <rumyra@gmail.com>
2021-05-08 14:03:32 +0100

    grid cleanup

--- app/codevember20/20esherTile.js
+++ app/codevember20/20esherTile.js
@@ -1,8 +1,10 @@
 console.clear();
 
 // I'm going to refactor the grid class - this is a breaking change
+// adding noise & distribution to grid
 
-import utils from '../../lib/utils.js'
+import utils from '../../lib/utils.js';
+import SimplexNoise from '../../lib/simplex.js';
 import { VizShape, VizCircle, VizRect, VizCross, VizPolar, VizFlower, VizDiamond, VizSVG, VizDrop, VizTri, VizEsher } from '../../lib/shape/VizShape.js';
 import VizTile from '../../lib/shape/VizTile.js'
 import Viz2d from '../../lib/canvas/Viz2d.js';
@@ -36,15 +38,16 @@ vizScreen.clear(palette.darkest.toHslString());
 const gridOptions = {
 	spacing: 0.6,
 	padding: false,
-	offset: 0.5,
+	offset: 0,
 	axis: 'x',
-	dis: [0, 0],
-	falloff: 0.5
+	dis: [0, 0.5],
+	falloff: [1, 1]
 }
 
 const grid = new VizGrid(vizScreen.width, vizScreen.height, gridOptions);
+console.log(vizScreen.width);
 
-console.log(grid.offset, grid._returnOrigSize() * grid.xCount);
+// console.log(grid.offset, grid._returnOrigSize() * grid.xCount);
 
 // invert gaus noise function
 let invertGaus = function() {
@@ -57,25 +60,48 @@ let invertGaus = function() {
 	return val;
 }
 
-grid.forEach((el, i) => {
+let size = grid.cw;
+let shapes = [];
+for (let i=0; i<1; i++) {
 
-	let currentTile = new VizTile();
-
-	currentTile.x = el.x;
-	currentTile.y = el.y;
-	currentTile.w = grid.cw;
-	currentTile.h = grid.ch;
-
-	let size = grid.cw;
-
-	currentTile.addShape(new VizEsher({
-		x: 0, y: 0, w: size,
+	let shape = new VizEsher({
+		x: i%2 === 0 ? 0.25 : 0.75,
+		y: i<2 ? 0.25 : 0.75,
+		w: size/2, h: size/2,
 		fill: 'transparent',
 		stroke: palette.removeDark[utils.randomInt(0, 4)],
 		strokeWidth: 4.0,
-		rotate: 90
-	}))
+		randomise: 0.8,
+		points: 1,
+		corner: false,
+		isCurved: true
+	})
+	shapes.push(shape);
+}
 
-	currentTile.update(vizScreen.ctx);
+let tile = new VizTile();
+tile.shapes = shapes;
+
+let simplex = new SimplexNoise('seed');
+let value2d = simplex.noise2D(1, 1);
+
+// el is {x, y} i is [cx, cy]
+grid.forEach((el, i) => {
+
+	let chanceVal = simplex.noise2D(i[0], i[1]);
+
+	tile.x = el.x;
+	tile.y = el.y;
+	tile.w = grid.cw;
+	tile.h = grid.ch;
+
+	// if (chanceVal > 0) {
+	// 	tile.update(vizScreen.ctx);
+	// }
+
+	// console.log(el.isAlive);
+	if (el.isAlive === true) {
+		tile.update(vizScreen.ctx)
+	}
 
 })
